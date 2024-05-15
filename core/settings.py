7830e0d65a14efe3a1918d018c34014eab142048
abuwcheek <abdulloistamov1034@gmail.com>
2024-05-15 10:14:20 +0500

    xatolar togrilandi

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3zu*2dqsj*5geomev0ig!5vog6wg4-_=)4z5608!e=nkz1^8+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_URL = '/static/'#Location of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('kechirasiz/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
-    path('home/', include('apps.home.urls'))
+    path('', include('apps.home.urls'))
 ]
 
 
